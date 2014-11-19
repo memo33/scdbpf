d4295bc45aa401c85047212d4f79d935aaef2411
memo <memo33@users.noreply.github.com>
2014-11-19 19:03:30 +0100

    adds function to create Preview Effects for puzzle pieces
    This is still experimental.

--- src/main/scala/scdbpf/Experimental.scala
+++ src/main/scala/scdbpf/Experimental.scala
@@ -56,4 +56,46 @@ object Experimental {
     def apply(x: Int, y: Int): RGBA = Fsh.conversions.argbToRGBA(img.getRGB(x, y))
   }
 
+  object PreviewEffect extends ((Int, String) => DbpfType) {
+    private val TemplateLength = 0x04F0
+    private lazy val templateData = {
+      import rapture.io._, resource._
+      managed(new ByteInput(getClass.getResourceAsStream("/preview_effect_template.eff"))) acquireAndGet { in =>
+        val arr = DbpfUtil.slurpBytes(in)
+        assert(arr.length == TemplateLength)
+        arr
+      }
+    }
+    private val IdIndex = Seq(0x00c8, 0x027a)
+    private val LengthIndex = Seq(0x03ae, 0x042d, 0x04a0, 0x04a8)
+
+    /** Constructs a puzzle piece preview effect directory file.
+      * @param iid the ID of the preview model
+      * @param refName the name by which the effect is referenced in RUL0; it
+      * will be converted to lower-case and prefixed with 'preview_' if not
+      * already.
+      */
+    def apply(iid: Int, refName: String) = new DbpfType {
+      private val name = {
+        val low = refName.toLowerCase
+        if (low.startsWith("preview_")) low else "preview_" + low
+      }
+      protected lazy val data = {
+        val buf = DbpfUtil.allocLEBB(templateData.length + 4 * name.length + 8)
+        def transfer(from: Int, to: Int) = buf.put(templateData, from, to - from)
+        val nameBytes = name.getBytes(DbpfUtil.asciiEncoding)
+        val nameBytesRed = (name + "_red").getBytes(DbpfUtil.asciiEncoding)
+        transfer(0,              IdIndex(0)) putInt iid
+        transfer(IdIndex(0) + 4, IdIndex(1)) putInt iid
+        transfer(IdIndex(1) + 4,     LengthIndex(0)) putInt name.length       put nameBytes
+        transfer(LengthIndex(0) + 4, LengthIndex(1)) putInt (name.length + 4) put nameBytesRed
+        transfer(LengthIndex(1) + 4, LengthIndex(2)) putInt name.length       put nameBytes
+        transfer(LengthIndex(2) + 4, LengthIndex(3)) putInt (name.length + 4) put nameBytesRed
+        transfer(LengthIndex(3) + 4, templateData.length)
+        assert(buf.remaining == 0)
+        buf.array
+      }
+    }
+  }
+
 }
---
+++ src/test/scala/scdbpf/ExperimentalSpec.scala
@@ -0,0 +1,15 @@
+package scdbpf
+
+import org.scalatest.{WordSpec, Matchers}
+import Experimental._
+
+class ExperimentalSpec extends WordSpec with Matchers {
+
+  "PreviewEffect" should {
+    "be constructible from resource" in {
+      val eff = PreviewEffect(0x12345678, "road_puzzlepiece002")
+      import rapture.core.strategy.throwExceptions
+      BufferedEntry(Tgi.Blank.copy(Tgi.EffDir), eff, compressed = true).toRawEntry
+    }
+  }
+}
