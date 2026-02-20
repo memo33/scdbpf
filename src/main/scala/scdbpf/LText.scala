4173d6f785bd7e961732ff2f3f13cdf7e58ad50f
memo <memo33@users.noreply.github.com>
2026-02-20 21:08:39 +0100

    add support for Utf8/Ascii for LText

--- src/main/scala/scdbpf/LText.scala
+++ src/main/scala/scdbpf/LText.scala
@@ -5,13 +5,22 @@ import java.nio.ByteBuffer
 import DbpfUtil._
 
 sealed trait LText extends DbpfType {
-
   val text: String
+  val format: LText.Format.Value
 }
 
 object LText extends DbpfTypeCompanion[LText] {
 
-  def apply(text: String): LText = new FreeLText(text)
+  object Format extends Enumeration {
+    val Utf16, Utf8, Ascii, AsciiNoHeader = Value
+    def toCharset(format: Format.Value): java.nio.charset.Charset = format match {
+      case Format.Utf16 => java.nio.charset.StandardCharsets.UTF_16LE
+      case Format.Utf8 => java.nio.charset.StandardCharsets.UTF_8
+      case Format.Ascii | Format.AsciiNoHeader => java.nio.charset.StandardCharsets.US_ASCII
+    }
+  }
+
+  def apply(text: String, format: Format.Value = Format.Utf16): LText = new FreeLText(text, format)
 
   implicit val converter: Converter[DbpfType, LText] = new Converter[DbpfType, LText] {
     def apply(from: DbpfType): LText = {
@@ -19,34 +28,61 @@ object LText extends DbpfTypeCompanion[LText] {
     }
   }
 
-  private val controlChar: Short = 0x1000
+  private val ControlChar16: Short = 0x1000
+  private val ControlChar8: Short = 0x0800
+  private val ControlChar0: Short = 0x0000
+
+  private def isAsciiPrintable(arr: Array[Byte], offset: Int): Boolean = {
+    var i = offset;
+    while (i < arr.length && { val c = arr(i); c >= 32 && c <= 126 || c == '\n'.toByte || c == '\r'.toByte || c == '\t'.toByte }) {
+      i += 1
+    }
+    i == arr.length
+  }
 
   private class BufferedLText(arr: Array[Byte]) extends RawType(arr) with LText {
-    val text: String = {
+    val (format, text): (Format.Value, String) = {
       val buf = wrapLEBB(data)
-      if (data.length < 4) {
-        throw new DbpfDecodeFailedException(f"length is only ${data.length}, minimum is 4 bytes")
+      var count = -1
+      var format = Format.AsciiNoHeader
+      if (data.length >= 4) {
+        count = buf.getShort().toInt & 0xffff
+        val cc = buf.getShort()
+        cc match {
+          case ControlChar16 => format = Format.Utf16
+          case ControlChar8 => format = Format.Utf8
+          case ControlChar0 => format = Format.Ascii
+          case _ => buf.position(0); count = -1; format = Format.AsciiNoHeader
+        }
       }
-      val expectedSize = data.length - 4
-      val count = buf.getShort() * 2
-      val cc = buf.getShort() // 0x1000
-      if (cc != controlChar) {
-        throw new DbpfDecodeFailedException(f"control character was 0x$cc%04X, expected 0x1000")
-      } else if (count != expectedSize) { // currently treats UTF-16 as fixed-width encoding
-        throw new DbpfDecodeFailedException(f"declared length was $count, expected $expectedSize")
+      if ((format == Format.AsciiNoHeader || format == Format.Ascii) && !isAsciiPrintable(data, buf.position())) {
+        throw new DbpfDecodeFailedException("bytes contain non-printable ASCII characters")
       }
-      new String(data, 4, expectedSize, "UTF-16LE")
+      val s = new String(data, buf.position(), data.length - buf.position(), Format.toCharset(format))
+      if (count != -1 && s.length != count) {
+        throw new DbpfDecodeFailedException(f"declared length was $count, expected ${s.length}")
+      }
+      (format, s)
     }
   }
 
-  private class FreeLText(val text: String) extends LText {
+  private class FreeLText(val text: String, val format: LText.Format.Value) extends LText {
     protected lazy val data: Array[Byte] = {
-      val buf = allocLEBB(4 + 2 * text.length)
-      buf.putShort(text.length.toShort)
-      buf.putShort(LText.controlChar)
-      buf.put(text.getBytes("UTF-16LE"))
-      assert(buf.remaining() == 0)
-      buf.array()
+      val bytes = text.getBytes(Format.toCharset(format))
+      if (format == Format.AsciiNoHeader) {
+        bytes
+      } else {
+        val buf = allocLEBB(4 + bytes.length)
+        buf.putShort(text.length.toShort)  // TODO support up to 200k characters
+        buf.putShort(format match {
+          case Format.Utf16 => ControlChar16
+          case Format.Utf8 => ControlChar8
+          case _ => ControlChar0
+        })
+        buf.put(bytes)
+        assert(buf.remaining() == 0)
+        buf.array()
+      }
     }
   }
 }
--- src/test/scala/scdbpf/LTextSpec.scala
+++ src/test/scala/scdbpf/LTextSpec.scala
@@ -9,13 +9,46 @@ class LTextSpec extends AnyWordSpec with Matchers {
   import strategy.throwExceptions
 
   "LText" should {
-    "encode/decode inversively" in {
-      val ltext = LText("abcdefgäöüßéèčñçαβγℕℝ∂¬∨∧∡∥→∞∅⇒↦Θ⇔⇐")
-      val e = BufferedEntry(Tgi.Blank, ltext: DbpfType, true)
+    def check(ltext: LText) = {
+      val e = BufferedEntry(Tgi.Blank, ltext: DbpfType, true).toRawEntry.toBufferedEntry
       val ltext2 = e.content.convertTo(LText)
       assert(ltext ne ltext2)
       ltext.text should be (ltext2.text)
       ltext.dataView.toSeq should be (ltext2.dataView.toSeq)
+      ltext.format should be (ltext2.format)
+    }
+    "encode/decode inversely (UTF-16)" in {
+      check(LText("abcdefgäöüßéèčñçαβγℕℝ∂¬∨∧∡∥→∞∅⇒↦Θ⇔⇐", format = LText.Format.Utf16))
+      check(LText("", format = LText.Format.Utf16))
+    }
+    "encode/decode inversely (UTF-8)" in {
+      check(LText("abcdefgäöüßéèčñçαβγℕℝ∂¬∨∧∡∥→∞∅⇒↦Θ⇔⇐", format = LText.Format.Utf8))
+      check(LText("", format = LText.Format.Utf8))
+    }
+    "encode/decode inversely (ASCII)" in {
+      check(LText("abcdefg0123XYZ~^_&", format = LText.Format.Ascii))
+      check(LText("", format = LText.Format.Ascii))
+    }
+    "encode/decode inversely (ASCII-no-header)" in {
+      check(LText("abcdefg0123XYZ~^_&", format = LText.Format.AsciiNoHeader))
+      check(LText("", format = LText.Format.AsciiNoHeader))
+    }
+    "not decode non-printable ASCII characters" in {
+      def except(raw: RawType) = {
+        intercept[DbpfDecodeFailedException](raw.convertTo(LText)).getMessage.shouldBe("bytes contain non-printable ASCII characters")
+      }
+      except(RawType(Array[Byte](1, 0, 0, 0, 0)))
+      except(RawType(Array[Byte](1, 0, 0, 0, 31)))
+      except(RawType(Array[Byte](1, 0, 0, 0, 127)))
+      except(RawType(Array[Byte](1, 0, 0, 0, 'ä'.toByte)))
+      val l1 = RawType(Array[Byte](0, 0, 0, 0)).convertTo(LText)
+      l1.format.shouldBe(LText.Format.Ascii)
+      l1.text.shouldBe("")
+      except(RawType(Array[Byte](31)))
+      except(RawType(Array[Byte](127)))
+      val l2 = RawType(Array[Byte](126)).convertTo(LText)
+      l2.format.shouldBe(LText.Format.AsciiNoHeader)
+      l2.text.shouldBe("~")
     }
   }
 }
